aeec9b86e5d76dbf41095f0e393f1bebbe28e2c0
john <aarontu803@gmail.com>
2023-11-08 20:59:13 +0800

    wip:activity layout

---
+++ src/components/LastActivity.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import bg from '@/assets/images/last-active.svg'
+import IrregularSvg from './IrregularSvg.vue'
+</script>
+
+<template>
+  <div class="bg position-relative overflow-hidden">
+    <!-- <img :src="bg"/> -->
+    <IrregularSvg
+      fill="rgb(255, 255, 255)"
+      opacity="1"
+      class="irregular position-absolute"
+    />
+    <div class="container">
+      <h3 class="fs-2 text-primary mb-2">About Me</h3>
+      <h2 class="display-1 text-secondary mb-20">立法委員<br />候選人主張</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.bg{
+  background: url(../assets/images/last-active.svg) ;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  padding-top: 150px;
+  padding-bottom: 195px;
+}
+.irregular {
+  width: 33%;
+  height: 32.5%;
+  transform: rotate(166.897deg);
+  top: -15%;
+  left: -5%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import BannerComp from '@/components/BannerComp.vue'
 import AboutComp from '@/components/AboutComp.vue'
+import LastActivity from '@/components/LastActivity.vue'
 </script>
 
 <template>
   <main>
     <BannerComp />
-    <AboutComp></AboutComp>
+    <AboutComp class="mb-257"></AboutComp>
+    <LastActivity></LastActivity>
   </main>
 </template>
 <style lang="scss" scoped>
+.mb-257{
+  margin-bottom: 257px;
+}
 </style>
