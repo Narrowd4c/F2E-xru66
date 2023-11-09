71e0b4e64250329038d6208ba9e973ca3366df13
john <aarontu803@gmail.com>
2023-11-10 06:01:08 +0800

    feat:donate layout

---
+++ src/components/DonateComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto">
+    <SectionTitle class="text-center mx-auto" sub-title="Donate" title="捐款方案"></SectionTitle>
+    <div class="width-fit hstack mx-auto">
+      <span>累計</span>
+      <span class="ms-2 text-primary font-jost display-4">100,000,000</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SectionTitle from './SectionTitle.vue'
+</script>
+
+<style scoped></style>
