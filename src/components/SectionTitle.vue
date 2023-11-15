234bce630322597196879f1a89dcc9f96ebfa769
john <aarontu803@gmail.com>
2023-11-16 04:07:26 +0800

    fix:subtitle padding

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -26,7 +26,7 @@ defineProps({
 
 <template>
   <div class="w-fit">
-    <h3 class="fs-2 text-primary mb-2">{{ subTitle }}</h3>
+    <h3 class="fs-2 text-primary mb-2 ps-2">{{ subTitle }}</h3>
     <h2 class="display-1 text-secondary title-max-width">{{ title }}</h2>
   </div>
 </template>
