74b41394f8403456effafd725c84e21783cedaad
john <aarontu803@gmail.com>
2023-11-21 19:25:49 +0800

    fix:og-image url

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,18 @@ useHead({
   link: [
     {
       rel: 'icon',
-      href: '/webicon.svg'
+      href: './webicon.svg'
     }
   ],
   meta: [
-    { property: 'og:image', content: ogImg },
+    { property: 'og:image', content: window.location.origin + ogImage },
     { property: 'og:image:width', content: 900 },
     { property: 'og:image:height', content: 450 },
     { property: 'og:title', content: '2024 喵立漢立委競選網站' },
     { property: 'og:description', content: '2024 喵立漢立委的政績政策介紹與小額贊助' },
     { name: 'description', content: '2024 喵立漢立委的政績政策介紹與小額贊助 ' },
-    { property: 'og:type', content: 'website' }
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: window.location.origin + import.meta.env.BASE_URL },
   ]
 })
 
